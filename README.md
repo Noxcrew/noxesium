08f1289b00b5ab2faee919743bcbfa5c5ca17501
Daniel Goossens <daniel.goossens@noxcrew.com>
2022-05-28 23:18:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Noxesium
+---
+A fabric mod with bugfixes and various performance improvements.
+
+### Performance
+- Optimized Beacon block entity rendering
+
+
+## Bugfixes
---
+++ src/main/java/com/noxcrew/noxesium/mixin/client/beacon/PreventDoubleBeaconRenderingMixin.java
@@ -0,0 +1,35 @@
+package com.noxcrew.noxesium.mixin.client.beacon;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
+import net.minecraft.world.level.block.entity.BeaconBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Set;
+
+/**
+ * Prevents beacons from being added to the renderable chunk entities. This is so we can do the
+ * entirety of beacon rendering custom for all beacons at once. We do add them to global entities and
+ * we catch them from there later.
+ */
+@Mixin(ChunkRenderDispatcher.RenderChunk.RebuildTask.class)
+public class PreventDoubleBeaconRenderingMixin {
+
+    @Inject(method = "handleBlockEntity", at = @At("HEAD"), cancellable = true)
+    public <E extends BlockEntity> void render(ChunkRenderDispatcher.CompiledChunk compiledChunk, Set<BlockEntity> set, E blockEntity, CallbackInfo ci) {
+        if (blockEntity instanceof BeaconBlockEntity) {
+            ci.cancel();
+
+            // Only add the beacon to the global entities
+            BlockEntityRenderer<E> blockEntityRenderer = Minecraft.getInstance().getBlockEntityRenderDispatcher().getRenderer(blockEntity);
+            if (blockEntityRenderer != null) {
+                set.add(blockEntity);
+            }
+        }
+    }
+}
