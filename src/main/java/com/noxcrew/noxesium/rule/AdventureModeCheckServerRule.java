15118347fffa32d509af80733c13f4edd243c270
Daniël Goossens <daniel@goossens.ch>
2022-08-02 11:08:24 +0200

    Allow server to define global CanDestroy and CanPlaceOn lists

---
+++ src/main/java/com/noxcrew/noxesium/rule/AdventureModeCheckServerRule.java
@@ -0,0 +1,26 @@
+package com.noxcrew.noxesium.rule;
+
+import net.minecraft.network.FriendlyByteBuf;
+
+import java.util.List;
+
+/**
+ * A server rule that stores a [CustomAdventureModeCheck].
+ */
+public class AdventureModeCheckServerRule extends ServerRule<CustomAdventureModeCheck> {
+
+    public AdventureModeCheckServerRule(int index) {
+        super(index);
+    }
+
+    @Override
+    public CustomAdventureModeCheck getDefault() {
+        return new CustomAdventureModeCheck(List.of());
+    }
+
+    @Override
+    public CustomAdventureModeCheck read(FriendlyByteBuf buffer) {
+        var stringList = buffer.readList(FriendlyByteBuf::readUtf);
+        return new CustomAdventureModeCheck(stringList);
+    }
+}
