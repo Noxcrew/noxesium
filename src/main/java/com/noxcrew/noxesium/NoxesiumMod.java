0aef8ad8eb09f6f8fa4a88ebb34c97556465dbb3
Daniël Goossens <daniel@goossens.ch>
2022-08-02 09:46:27 +0200

    Send GUI scale to server through network channel
    This allows servers to change part of their UI based on the GUI scale of the client, exact resolution isn't included atm

--- src/main/java/com/noxcrew/noxesium/NoxesiumMod.java
+++ src/main/java/com/noxcrew/noxesium/NoxesiumMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 
 /**
@@ -18,14 +19,43 @@ public class NoxesiumMod implements ClientModInitializer {
     public static final int VERSION = 0;
 
     public static final ResourceLocation CLIENT_INFORMATION_CHANNEL = new ResourceLocation("noxesium", "client_information");
+    public static final ResourceLocation CLIENT_SETTINGS_CHANNEL = new ResourceLocation("noxesium", "client_settings");
+
+    public static boolean connected = false;
 
     @Override
     public void onInitializeClient() {
         // Every time the client joins a server we send over information on the version being used
         ClientPlayConnectionEvents.JOIN.register((ignored1, ignored2, ignored3) -> {
-            var outBuffer = PacketByteBufs.create();
-            outBuffer.writeInt(VERSION);
-            ClientPlayNetworking.send(CLIENT_INFORMATION_CHANNEL, outBuffer);
+            // Send a packet containing information about the client version of Noxesium
+            {
+                var outBuffer = PacketByteBufs.create();
+                outBuffer.writeInt(VERSION);
+                ClientPlayNetworking.send(CLIENT_INFORMATION_CHANNEL, outBuffer);
+            }
+
+            // Store that we've connected and are able to send more information
+            connected = true;
+
+            // Inform the player about the GUI scale of the client
+            syncGuiScale();
         });
+
+        // Break the connection again on disconnection
+        ClientPlayConnectionEvents.DISCONNECT.register((ignored1, ignored2) -> {
+            connected = false;
+        });
+    }
+
+    /**
+     * Sends a packet to the server containing the GUI scale of the client which
+     * allows servers to more accurately adapt their UI to clients.
+     */
+    public static void syncGuiScale() {
+        if (!connected) return;
+        var outBuffer = PacketByteBufs.create();
+        outBuffer.writeInt(Minecraft.getInstance().options.guiScale().get());
+        outBuffer.writeBoolean(Minecraft.getInstance().isEnforceUnicode());
+        ClientPlayNetworking.send(CLIENT_SETTINGS_CHANNEL, outBuffer);
     }
 }
