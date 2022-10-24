058541064b8f530dbb9a7e5d95188de6935aaa6c
Daniël Goossens <daniel@goossens.ch>
2022-10-24 21:35:56 +0200

    Change HITW fix to meet community consensus on previous fix

---
+++ src/main/java/com/noxcrew/noxesium/mixin/client/BlockStateBaseMixin.java
@@ -0,0 +1,27 @@
+package com.noxcrew.noxesium.mixin.client;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.piston.MovingPistonBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockBehaviour.BlockStateBase.class)
+public abstract class BlockStateBaseMixin {
+
+    @Shadow public abstract Block getBlock();
+
+    @Inject(method = "isCollisionShapeFullBlock", at = @At(value = "HEAD"), cancellable = true)
+    private void redirected(BlockGetter blockGetter, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
+        // Enforce moving piston blocks to not have a full collision shape so
+        // they don't cast shadows on neighboring blocks
+        if (getBlock() instanceof MovingPistonBlock) {
+            cir.setReturnValue(false);
+        }
+    }
+}
