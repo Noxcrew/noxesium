16e2c285bda3bd877e67be9b3ee99d4ca2f162ac
Lucy Poulton <lucy.poulton@noxcrew.com>
2023-02-12 13:47:31 +0000

    Add `HELD_ITEM_NAME_OFFSET` server rule

--- src/main/java/com/noxcrew/noxesium/mixin/client/GuiMixin.java
+++ src/main/java/com/noxcrew/noxesium/mixin/client/GuiMixin.java
@@ -1,5 +1,6 @@
 package com.noxcrew.noxesium.mixin.client;
 
+import com.noxcrew.noxesium.rule.ServerRules;
 import net.minecraft.client.gui.Gui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -12,13 +13,11 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(Gui.class)
 public class GuiMixin {
 
-    private static final int ADJUST_BY = 5;
-
     @ModifyConstant(
             method = "renderSelectedItemName(Lcom/mojang/blaze3d/vertex/PoseStack;)V",
             constant = @Constant(intValue = 59)
     )
     public int modify(int constant) {
-        return constant + ADJUST_BY;
+        return constant + ServerRules.HELD_ITEM_NAME_OFFSET.get();
     }
 }
--- src/main/java/com/noxcrew/noxesium/rule/ServerRules.java
+++ src/main/java/com/noxcrew/noxesium/rule/ServerRules.java
@@ -21,4 +21,10 @@ public class ServerRules {
      * allowing the server to define which blocks are breakable globally regardless of tool.
      */
     public static ServerRule<CustomAdventureModeCheck> GLOBAL_CAN_DESTROY = new AdventureModeCheckServerRule(2);
+
+    /**
+     * An integer pixel amount to vertically offset the HUD held item name.
+     * useful for avoiding overlapping faction icons. Positive values move the text up.
+     */
+    public static ServerRule<Integer> HELD_ITEM_NAME_OFFSET = new IntegerServerRule(3, 0);
 }
