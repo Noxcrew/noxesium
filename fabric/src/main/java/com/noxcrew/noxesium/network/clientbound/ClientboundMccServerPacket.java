427c65be219c84c41893aa35b4f480d4c21e7461
Daniël Goossens <daniel@goossens.ch>
2023-08-06 20:02:46 +0200

    Add the legacy creatorbash game key

--- fabric/src/main/java/com/noxcrew/noxesium/network/clientbound/ClientboundMccServerPacket.java
+++ fabric/src/main/java/com/noxcrew/noxesium/network/clientbound/ClientboundMccServerPacket.java
@@ -15,11 +15,19 @@ public class ClientboundMccServerPacket extends ClientboundNoxesiumPacket {
     public final String subType;
     public final String associatedGame;
 
+    /**
+     * The legacy id of this game, these are only used by the event and mcc.live.
+     * These are provided for completeness and comparison, but you should generally use
+     * the regular associatedGame value on Island.
+     */
+    public final String legacyGameId;
+
     public ClientboundMccServerPacket(FriendlyByteBuf buf) {
         super(buf.readVarInt());
         this.type = buf.readUtf();
         this.subType = buf.readUtf();
         this.associatedGame = buf.readUtf();
+        this.legacyGameId = buf.readUtf();
     }
 
     @Override
