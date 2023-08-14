793cebe02fd0af455dba94fa8fc95f666c84c0ad
Daniël Goossens <daniel@goossens.ch>
2023-08-14 11:37:27 +0200

    Update to Sodium 0.5.1

--- fabric/src/main/java/com/noxcrew/noxesium/mixin/beacon/SodiumWorldRendererMixin.java
+++ fabric/src/main/java/com/noxcrew/noxesium/mixin/beacon/SodiumWorldRendererMixin.java
@@ -5,24 +5,23 @@ import com.noxcrew.noxesium.render.GlobalBlockEntityRenderer;
 import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import me.jellysquid.mods.sodium.client.render.chunk.RenderSectionManager;
-import me.jellysquid.mods.sodium.client.render.chunk.data.BuiltSectionInfo;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderBuffers;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.server.level.BlockDestructionProgress;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
 
 /**
  * Hooks into Sodium's WorldRenderer logic for rendering global block entities (which all beacons are)
@@ -42,8 +41,8 @@ public class SodiumWorldRendererMixin {
         // Remove the normal method. We add a custom call because we need the camera instance.
     }
 
-    @Inject(method = "renderTileEntities", at = @At(value = "TAIL"))
-    private void renderGlobalBlockEntities(PoseStack poseStack, RenderBuffers renderBuffers, Long2ObjectMap<SortedSet<BlockDestructionProgress>> blockBreakingProgressions, Camera camera, float tickDelta, CallbackInfo ci) {
+    @Inject(method = "renderBlockEntities(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/RenderBuffers;Lit/unimi/dsi/fastutil/longs/Long2ObjectMap;Lnet/minecraft/client/Camera;F)V", at = @At(value = "TAIL"))
+    private void renderBlockEntities(PoseStack poseStack, RenderBuffers renderBuffers, Long2ObjectMap<SortedSet<BlockDestructionProgress>> blockBreakingProgressions, Camera camera, float tickDelta, CallbackInfo ci) {
         Set<BlockEntity> globalBlockEntities = null;
         var iterator = renderSectionManager.getSectionsWithGlobalEntities().iterator();
         outer: while (true) {
