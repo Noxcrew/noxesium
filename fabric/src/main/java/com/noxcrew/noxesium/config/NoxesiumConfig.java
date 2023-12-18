4c86d039e208c2eed6d216eff902165fcbcb965d
Aeltumn <daniel@goossens.ch>
2023-12-18 22:56:45 +0100

    Make option instances transient

--- fabric/src/main/java/com/noxcrew/noxesium/config/NoxesiumConfig.java
+++ fabric/src/main/java/com/noxcrew/noxesium/config/NoxesiumConfig.java
@@ -28,7 +28,7 @@ public class NoxesiumConfig {
     public boolean enableExperimentalPerformancePatches = false;
 
     // Also create vanilla option instances to use in the video settings menu
-    public OptionInstance<Boolean> vanillaExperimentalPatches = OptionInstance.createBoolean(
+    public transient OptionInstance<Boolean> vanillaExperimentalPatches = OptionInstance.createBoolean(
             "noxesium.options.experimental_patches.name",
             OptionInstance.cachedConstantTooltip(Component.translatable("noxesium.options.experimental_patches.tooltip")),
             hasConfiguredPerformancePatches(),
@@ -37,7 +37,7 @@ public class NoxesiumConfig {
                 save();
             }
     );
-    public OptionInstance<Boolean> vanillaFpsOverlay = OptionInstance.createBoolean(
+    public transient OptionInstance<Boolean> vanillaFpsOverlay = OptionInstance.createBoolean(
             "noxesium.options.fps_overlay.name",
             OptionInstance.cachedConstantTooltip(Component.translatable("noxesium.options.fps_overlay.tooltip")),
             showFpsOverlay,
