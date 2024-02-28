60af5a9ccff064a42277d492205a32b331375a34
Michael H. <michael@iptables.sh>
2024-02-28 15:48:48 +0100

    Additional POM info

--- buildSrc/src/main/kotlin/noxesium.publishing.gradle.kts
+++ buildSrc/src/main/kotlin/noxesium.publishing.gradle.kts
@@ -20,6 +20,29 @@ publishing {
         create<MavenPublication>("maven") {
             version = "${property("mod_version")}"
             from(components["java"])
+            pom {
+                name = "Noxesium"
+                description = "A client-side fabric mod that improves performance and play experience on large multiplayer servers."
+                url = "https://github.com/Noxcrew/noxesium"
+                scm {
+                    url = "https://github.com/Noxcrew/noxesium"
+                    connection = "scm:git:https://github.com/Noxcrew/noxesium.git"
+                    developerConnection = "scm:git:https://github.com/Noxcrew/noxesium.git"
+                }
+                licenses {
+                    license {
+                        name = "LGPL-3.0"
+                        url = "https://www.gnu.org/licenses/lgpl-3.0.html"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "noxcrew"
+                        name = "Noxcrew"
+                        email = "contact@noxcrew.com"
+                    }
+                }
+            }
         }
     }
 }
