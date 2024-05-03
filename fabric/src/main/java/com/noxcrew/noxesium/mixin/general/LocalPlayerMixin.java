881dea9d4e3f82de66be2e3bc00f22642cdcf11f
Bawnorton <bawnorton@me.com>
2024-05-03 13:43:30 -0400

    replace redirects that call the target with equivalent wraps

--- fabric/src/main/java/com/noxcrew/noxesium/mixin/general/LocalPlayerMixin.java
+++ fabric/src/main/java/com/noxcrew/noxesium/mixin/general/LocalPlayerMixin.java
@@ -1,22 +1,20 @@
 package com.noxcrew.noxesium.mixin.general;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.noxcrew.noxesium.NoxesiumMod;
-import net.minecraft.client.KeyMapping;
 import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * Makes resetting of toggle keys on respawn configurable in the accessibility settings.
  */
 @Mixin(LocalPlayer.class)
-public class LocalPlayerMixin {
+public abstract class LocalPlayerMixin {
 
-    @Redirect(method = "respawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;resetToggleKeys()V"))
-    public void onResetToggleKeys() {
-        if (NoxesiumMod.getInstance().getConfig().resetToggleKeys) {
-            KeyMapping.resetToggleKeys();
-        }
+    @WrapWithCondition(method = "respawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;resetToggleKeys()V"))
+    public boolean onResetToggleKeys() {
+        return NoxesiumMod.getInstance().getConfig().resetToggleKeys;
     }
 }
