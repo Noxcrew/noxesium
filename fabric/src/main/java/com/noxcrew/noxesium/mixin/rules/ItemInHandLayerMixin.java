547ede03cd5a14e1a4ddda387993f2efbe2f29b3
Bawnorton <bawnorton@me.com>
2024-05-13 16:43:45 -0400

    Replace @Redirects that call the target with equivalent wraps (#62)
    Replace redirects that call the target with equivalent wraps

--- fabric/src/main/java/com/noxcrew/noxesium/mixin/rules/ItemInHandLayerMixin.java
+++ fabric/src/main/java/com/noxcrew/noxesium/mixin/rules/ItemInHandLayerMixin.java
@@ -1,5 +1,7 @@
 package com.noxcrew.noxesium.mixin.rules;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.noxcrew.noxesium.feature.rule.InventoryHelper;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.world.entity.LivingEntity;
@@ -13,14 +15,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  * Overrides the item shown as being held in the main hand on the 3d player model.
  */
 @Mixin(ItemInHandLayer.class)
-public class ItemInHandLayerMixin {
+public abstract class ItemInHandLayerMixin {
 
-    @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getMainHandItem()Lnet/minecraft/world/item/ItemStack;"))
-    public ItemStack getSelected(LivingEntity instance) {
+    @WrapOperation(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getMainHandItem()Lnet/minecraft/world/item/ItemStack;"))
+    public ItemStack getSelected(LivingEntity instance, Operation<ItemStack> original) {
         if (instance instanceof Player player) {
             return InventoryHelper.getRealSelected(player.getInventory());
         } else {
-            return instance.getMainHandItem();
+            return original.call(instance);
         }
     }
 }
--- fabric/src/main/java/com/noxcrew/noxesium/mixin/rules/ItemInHandRendererMixin.java
+++ fabric/src/main/java/com/noxcrew/noxesium/mixin/rules/ItemInHandRendererMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  * Overrides the item shown as being held in the main hand.
  */
 @Mixin(ItemInHandRenderer.class)
-public class ItemInHandRendererMixin {
+public abstract class ItemInHandRendererMixin {
 
     @Redirect(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;getMainHandItem()Lnet/minecraft/world/item/ItemStack;"))
     public ItemStack getSelected(LocalPlayer instance) {
