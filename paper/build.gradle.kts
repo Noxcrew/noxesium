db22ad7d05f130de45cfee98096a4c938973e874
Aeltumn <daniel@goossens.ch>
2024-06-06 14:24:36 +0200

    Back to Kotlin 1.9.24

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "1.9.24"
     id("io.papermc.paperweight.userdev") version "1.7.1"
 }
 
