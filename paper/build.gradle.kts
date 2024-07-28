ddeec1c9e5d7a3128b674cb029d69b16d22a799f
Aeltumn <daniel@goossens.ch>
2024-07-28 16:35:19 +0200

    Bump paper version to 1.21

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.20.6-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
     compileOnlyApi(libs.guava)
 
     api(libs.kotlin.coroutines)
