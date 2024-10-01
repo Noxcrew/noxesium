b67f67bce0d71b49066e4ae9702df204c832aae3
Aeltumn <daniel@goossens.ch>
2024-10-01 21:24:35 +0200

    Fixed qibs not getting registered properly #105

--- fabric/src/main/java/com/noxcrew/noxesium/mixin/rules/qib/SpatialEntityMixin.java
+++ fabric/src/main/java/com/noxcrew/noxesium/mixin/rules/qib/SpatialEntityMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.Interaction;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,11 +17,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Entity.class)
 public abstract class SpatialEntityMixin {
 
+    @Unique
+    private boolean noxesium$initialized = false;
+
     @Shadow
     public abstract AABB getBoundingBox();
 
+    @Inject(method = "setId", at = @At("RETURN"))
+    public void setId(int i, CallbackInfo ci) {
+        // When the entity id is set we trigger a spatial container update!
+        if (((Object) this) instanceof Interaction interaction) {
+            noxesium$initialized = true;
+            SpatialInteractionEntityTree.update(interaction);
+        }
+    }
+
     @Inject(method = "setBoundingBox", at = @At("HEAD"))
     public void onUpdateBoundingBox(AABB aABB, CallbackInfo ci) {
+        // Ignore updates until the entity id has been set
+        if (!noxesium$initialized) return;
+
         // Ignore if we're already at the exact same position!
         if (((Object) this) instanceof Interaction interaction && !aABB.equals(getBoundingBox())) {
             SpatialInteractionEntityTree.update(interaction);
