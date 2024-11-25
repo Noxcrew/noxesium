bfa6e91d30f4a00feec985b7c9f8aa7939c7cc58
Aeltumn <daniel@goossens.ch>
2024-11-25 23:12:19 +0100

    As usual it was user error

--- buildSrc/src/main/kotlin/multiloader.loader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader.loader.gradle.kts
@@ -18,23 +18,13 @@ dependencies {
 }
 
 tasks {
-    // Janky task to just copy over the resources folder
-    val copyResourcesFromCommon by tasks.registering(Copy::class) {
-        from(project(":common").file("src/main/resources"))
-        into("src/main/resources")
-        include("**/*")
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE
-    }
-
     named<JavaCompile>("compileJava").configure {
         dependsOn(configurations.getByName("commonJava"))
         source(configurations.getByName("commonJava"))
     }
     named<ProcessResources>("processResources").configure {
-        dependsOn(copyResourcesFromCommon)
         dependsOn(configurations.getByName("commonResources"))
         from(configurations.getByName("commonResources"))
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE
     }
     named<Jar>("sourcesJar").configure {
         dependsOn(configurations.getByName("commonJava"))
