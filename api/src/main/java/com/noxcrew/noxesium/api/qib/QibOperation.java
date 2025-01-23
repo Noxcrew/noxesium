b0fc091082813aca746e8a1f081a11818867e283
Gravy Boat <gravy@thatgravyboat.tech>
2025-01-23 12:28:50 -0330

    Add javadocs and bump protocol version

--- api/src/main/java/com/noxcrew/noxesium/api/qib/QibOperation.java
+++ api/src/main/java/com/noxcrew/noxesium/api/qib/QibOperation.java
@@ -1,5 +1,8 @@
 package com.noxcrew.noxesium.api.qib;
 
+/**
+ * Represents a mathematical operation/expression that can be applied to a value.
+ */
 public enum QibOperation {
     ADD,
     SET,
@@ -9,6 +12,22 @@ public enum QibOperation {
     MAX
     ;
 
+    /**
+     * Applies the operation to the given value with the given modifier resulting in the new value.
+     * <p/>
+     * The operation is applied as follows:
+     * <ul>
+     *     <li>{@link #ADD}: value + modifier</li>
+     *     <li>{@link #SET}: modifier</li>
+     *     <li>{@link #MUL}: value * modifier</li>
+     *     <li>{@link #DIV}: value / modifier</li>
+     *     <li>{@link #MIN}: min(value, modifier)</li>
+     *     <li>{@link #MAX}: max(value, modifier)</li>
+     * </ul>
+     * @param value the value to apply the operation to
+     * @param modifier the modifier to apply to the value
+     * @return the result of the operation
+     */
     public double apply(double value, double modifier) {
         return switch (this) {
             case ADD -> value + modifier;
