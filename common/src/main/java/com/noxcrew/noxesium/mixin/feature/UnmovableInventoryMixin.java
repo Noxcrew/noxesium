d2e0d304a794938f62039565af53020c13fb3570
LukynkaCZE <jiraklukas058@seznam.cz>
2025-05-28 17:27:48 +0200

    Add support for `noxesium:immovable` not having to be in `PublicBukkitValues` compound tag in custom data component

--- common/src/main/java/com/noxcrew/noxesium/mixin/feature/UnmovableInventoryMixin.java
+++ common/src/main/java/com/noxcrew/noxesium/mixin/feature/UnmovableInventoryMixin.java
@@ -1,22 +1,23 @@
 package com.noxcrew.noxesium.mixin.feature;
 
-import static com.noxcrew.noxesium.api.NoxesiumReferences.BUKKIT_COMPOUND_ID;
-import static com.noxcrew.noxesium.api.NoxesiumReferences.IMMOVABLE_TAG;
-
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomData;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static com.noxcrew.noxesium.api.NoxesiumReferences.BUKKIT_COMPOUND_ID;
+import static com.noxcrew.noxesium.api.NoxesiumReferences.IMMOVABLE_TAG;
+
 /**
  * Mixin for preventing items dropped from the hotbar.
- * This only prevent items with the {@link com.noxcrew.noxesium.api.NoxesiumReferences#IMMOVABLE_TAG} from being moved.
+ * This only prevents items with the {@link com.noxcrew.noxesium.api.NoxesiumReferences#IMMOVABLE_TAG} from being moved.
  */
 @Mixin(Inventory.class)
 public abstract class UnmovableInventoryMixin {
@@ -29,11 +30,12 @@ public abstract class UnmovableInventoryMixin {
             final boolean bl, final CallbackInfoReturnable<ItemStack> cir, @Local final ItemStack stack) {
         final CustomData data = stack.get(DataComponents.CUSTOM_DATA);
         if (data == null) return;
+
         final CompoundTag tag = data.getUnsafe();
         if (tag == null) return;
 
-        final CompoundTag bukkit = tag.getCompound(BUKKIT_COMPOUND_ID).orElse(null);
-        if (bukkit == null || !bukkit.contains(IMMOVABLE_TAG)) return;
+        @Nullable CompoundTag immovableTagContainer = tag.contains(IMMOVABLE_TAG) ? tag : tag.getCompound(BUKKIT_COMPOUND_ID).orElse(null);
+        if (immovableTagContainer == null || !immovableTagContainer.contains(IMMOVABLE_TAG)) return;
 
         cir.setReturnValue(ItemStack.EMPTY);
     }
