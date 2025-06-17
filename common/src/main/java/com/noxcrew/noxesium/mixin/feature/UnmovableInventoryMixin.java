4d85efc9d3b7f0afe149718c0f1ab38453cdff23
Aeltumn <daniel@goossens.ch>
2025-06-17 23:16:52 +0200

    Implement #135 with shared helper function

--- common/src/main/java/com/noxcrew/noxesium/mixin/feature/UnmovableInventoryMixin.java
+++ common/src/main/java/com/noxcrew/noxesium/mixin/feature/UnmovableInventoryMixin.java
@@ -1,14 +1,9 @@
 package com.noxcrew.noxesium.mixin.feature;
 
-import static com.noxcrew.noxesium.api.NoxesiumReferences.BUKKIT_COMPOUND_ID;
-import static com.noxcrew.noxesium.api.NoxesiumReferences.IMMOVABLE_TAG;
-
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.nbt.CompoundTag;
+import com.noxcrew.noxesium.feature.ImmovableTag;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.CustomData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Mixin for preventing items dropped from the hotbar.
- * This only prevent items with the {@link com.noxcrew.noxesium.api.NoxesiumReferences#IMMOVABLE_TAG} from being moved.
  */
 @Mixin(Inventory.class)
 public abstract class UnmovableInventoryMixin {
@@ -27,14 +21,8 @@ public abstract class UnmovableInventoryMixin {
             cancellable = true)
     public void preventMovingImmovables(
             final boolean bl, final CallbackInfoReturnable<ItemStack> cir, @Local final ItemStack stack) {
-        final CustomData data = stack.get(DataComponents.CUSTOM_DATA);
-        if (data == null) return;
-        final CompoundTag tag = data.getUnsafe();
-        if (tag == null) return;
-
-        final CompoundTag bukkit = tag.getCompound(BUKKIT_COMPOUND_ID).orElse(null);
-        if (bukkit == null || !bukkit.contains(IMMOVABLE_TAG)) return;
-
-        cir.setReturnValue(ItemStack.EMPTY);
+        if (ImmovableTag.isImmovable(stack)) {
+            cir.setReturnValue(ItemStack.EMPTY);
+        }
     }
 }
