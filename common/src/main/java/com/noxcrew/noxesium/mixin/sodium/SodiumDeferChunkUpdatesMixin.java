61061e4cc27dae667e5b2af81fc0e2b081062f96
Aeltumn <daniel@goossens.ch>
2025-12-27 23:27:49 +0100

    Update Sodium integration

--- common/src/main/java/com/noxcrew/noxesium/mixin/sodium/SodiumDeferChunkUpdatesMixin.java
+++ common/src/main/java/com/noxcrew/noxesium/mixin/sodium/SodiumDeferChunkUpdatesMixin.java
@@ -3,9 +3,10 @@ package com.noxcrew.noxesium.mixin.sodium;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.noxcrew.noxesium.feature.rule.ServerRules;
-import net.caffeinemc.mods.sodium.client.gui.SodiumGameOptions;
+import net.caffeinemc.mods.sodium.client.gui.SodiumOptions;
 import net.caffeinemc.mods.sodium.client.render.chunk.DeferMode;
 import net.caffeinemc.mods.sodium.client.render.chunk.RenderSectionManager;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,9 +24,10 @@ public class SodiumDeferChunkUpdatesMixin {
                     @At(
                             value = "FIELD",
                             target =
-                                    "Lnet/caffeinemc/mods/sodium/client/gui/SodiumGameOptions$PerformanceSettings;chunkBuildDeferMode:Lnet/caffeinemc/mods/sodium/client/render/chunk/DeferMode;"))
+                                    "Lnet/caffeinemc/mods/sodium/client/gui/SodiumOptions$PerformanceSettings;chunkBuildDeferMode:Lnet/caffeinemc/mods/sodium/client/render/chunk/DeferMode;",
+                            opcode = Opcodes.GETFIELD))
     private static DeferMode overrideDeferChunkUpdates(
-            SodiumGameOptions.PerformanceSettings instance, Operation<DeferMode> original) {
+            SodiumOptions.PerformanceSettings instance, Operation<DeferMode> original) {
         if (ServerRules.DISABLE_DEFERRED_CHUNK_UPDATES.getValue()) {
             return DeferMode.ZERO_FRAMES;
         }
