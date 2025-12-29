f3c2b8ceaa2f1ebc4793ba64bc55eca37cb60390
Aeltumn <daniel@goossens.ch>
2025-12-29 01:20:00 +0100

    Fix iris support for text display background patch

--- fabric/src/main/java/com/noxcrew/noxesium/core/fabric/mixin/feature/FixTextDisplayTransparencyMixin.java
+++ fabric/src/main/java/com/noxcrew/noxesium/core/fabric/mixin/feature/FixTextDisplayTransparencyMixin.java
@@ -1,30 +1,36 @@
 package com.noxcrew.noxesium.core.fabric.mixin.feature;
 
-import com.noxcrew.noxesium.core.fabric.feature.render.CustomRenderTypes;
-import net.minecraft.client.renderer.entity.DisplayRenderer;
-import net.minecraft.client.renderer.rendertype.RenderType;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.platform.DepthTestFunction;
+import net.minecraft.client.renderer.RenderPipelines;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(DisplayRenderer.TextDisplayRenderer.class)
+@Mixin(RenderPipelines.class)
 public abstract class FixTextDisplayTransparencyMixin {
-
-    /**
-     * Normal text backgrounds like on name tags render the background as see-through and the text as whatever the selected mode is. However,
-     * text displays render the background as non-see-through. We fix this by just always rendering the backgrounds as see-through
-     * which fixes issues with its transparency. We do however use a custom type with a depth buffer that ensures the background does not
-     * render through walls. We want to render things behind it, not it behind other things.
-     */
-    @Redirect(
-            method =
-                    "submitInner(Lnet/minecraft/client/renderer/entity/state/TextDisplayEntityRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;IF)V",
+    @WrapOperation(
+            method = "<clinit>",
             at =
                     @At(
                             value = "INVOKE",
                             target =
-                                    "Lnet/minecraft/client/renderer/rendertype/RenderTypes;textBackground()Lnet/minecraft/client/renderer/rendertype/RenderType;"))
-    public RenderType determineRenderType() {
-        return CustomRenderTypes.textBackgroundSeeThroughWithDepth();
+                                    "Lcom/mojang/blaze3d/pipeline/RenderPipeline$Builder;build()Lcom/mojang/blaze3d/pipeline/RenderPipeline;"))
+    private static RenderPipeline onBuildPipeline(RenderPipeline.Builder instance, Operation<RenderPipeline> original) {
+        if (instance.location.isPresent()) {
+            var path = instance.location.get().getPath();
+            if (path.equals("pipeline/text_background")) {
+                /*
+                 * MC-259812:
+                 * Normal text backgrounds like on name tags render the background as see-through and the text as whatever the selected mode is. However,
+                 * text displays render the background as non-see-through. We fix this by just always rendering the backgrounds as see-through
+                 * which fixes issues with its transparency. We modify the vanilla type so Iris doesn't break.
+                 */
+                instance.withDepthWrite(false);
+                instance.withDepthTestFunction(DepthTestFunction.LEQUAL_DEPTH_TEST);
+            }
+        }
+        return original.call(instance);
     }
 }
