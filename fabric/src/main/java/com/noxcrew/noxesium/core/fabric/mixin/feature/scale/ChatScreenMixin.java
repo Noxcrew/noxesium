cbdf4ea1689a276ba414349ef558ae822a83a49d
i5wear <103974588+i5wear@users.noreply.github.com>
2026-03-13 18:57:41 +0800

    Fix chat scaling (#179)
    * Simplified window extension
    
    * Corrected variable name
    
    * Fixes #171

---
+++ fabric/src/main/java/com/noxcrew/noxesium/core/fabric/mixin/feature/scale/ChatScreenMixin.java
@@ -0,0 +1,27 @@
+package com.noxcrew.noxesium.core.fabric.mixin.feature.scale;
+
+import com.noxcrew.noxesium.core.fabric.NoxesiumMod;
+import com.noxcrew.noxesium.core.feature.GuiElement;
+import net.minecraft.client.gui.screens.ChatScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(ChatScreen.class)
+public class ChatScreenMixin {
+
+    @ModifyArg(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ActiveTextCollector$ClickableStyleFinder;<init>(Lnet/minecraft/client/gui/Font;II)V"), index = 1)
+    public int onMousePositionX(int original) {
+        return (int) (original / NoxesiumMod.getInstance().getConfig().getScale(GuiElement.CHAT));
+    }
+
+    @ModifyArg(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ActiveTextCollector$ClickableStyleFinder;<init>(Lnet/minecraft/client/gui/Font;II)V"), index = 2)
+    public int onMousePositionY(int original) {
+        return (int) (original / NoxesiumMod.getInstance().getConfig().getScale(GuiElement.CHAT));
+    }
+
+    @ModifyArg(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/ChatComponent;captureClickableText(Lnet/minecraft/client/gui/ActiveTextCollector;IIZ)V"), index = 1)
+    public int onScreenHeight(int original) {
+        return (int) (original / NoxesiumMod.getInstance().getConfig().getScale(GuiElement.CHAT));
+    }
+}
--- fabric/src/main/java/com/noxcrew/noxesium/core/fabric/mixin/feature/scale/WindowExt.java
+++ fabric/src/main/java/com/noxcrew/noxesium/core/fabric/mixin/feature/scale/WindowExt.java
@@ -1,49 +1,35 @@
 package com.noxcrew.noxesium.core.fabric.mixin.feature.scale;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.blaze3d.platform.Window;
 import com.noxcrew.noxesium.core.fabric.NoxesiumMod;
 import com.noxcrew.noxesium.core.fabric.feature.ScalingExtension;
 import com.noxcrew.noxesium.core.feature.GuiElement;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Window.class)
 public abstract class WindowExt implements ScalingExtension {
-    @Shadow
-    public abstract int getGuiScaledWidth();
-
-    @Shadow
-    public abstract int getGuiScaledHeight();
 
     @Unique
-    private Integer noxesium$guiWidth = null;
-
-    @Unique
-    private Integer noxesium$guiHeight = null;
+    private float noxesium$divisor = 1;
 
     @Override
     public void noxesium$whileRescaled(GuiElement element, Runnable function) {
         var config = NoxesiumMod.getInstance().getConfig();
-        var scale = (float) config.getScale(element);
-        var oldWidth = noxesium$guiWidth;
-        var oldHeight = noxesium$guiHeight;
-        noxesium$guiWidth = (int) (getGuiScaledWidth() / scale);
-        noxesium$guiHeight = (int) (getGuiScaledHeight() / scale);
+        noxesium$divisor = (float) config.getScale(element);
         function.run();
-        noxesium$guiWidth = oldWidth;
-        noxesium$guiHeight = oldHeight;
+        noxesium$divisor = 1;
     }
 
-    @WrapMethod(method = "getGuiScaledWidth")
-    public int wrapGetWidth(Operation<Integer> original) {
-        return noxesium$guiWidth == null ? original.call() : noxesium$guiWidth;
+    @ModifyReturnValue(method = "getGuiScaledWidth", at= @At("TAIL"))
+    public int wrapGetWidth(int original) {
+        return (int) (original / noxesium$divisor);
     }
 
-    @WrapMethod(method = "getGuiScaledHeight")
-    public int wrapGetHeight(Operation<Integer> original) {
-        return noxesium$guiHeight == null ? original.call() : noxesium$guiHeight;
+    @ModifyReturnValue(method = "getGuiScaledHeight", at= @At("TAIL"))
+    public int wrapGetHeight(int original) {
+        return (int) (original / noxesium$divisor);
     }
 }
