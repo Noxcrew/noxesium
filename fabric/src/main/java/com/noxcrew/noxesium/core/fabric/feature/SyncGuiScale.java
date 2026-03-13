f7a5bbfbb4a689ca6e40089f108581c9100658cf
Aeltumn <daniel@goossens.ch>
2026-03-13 14:26:26 +0100

    feature: Reduce bandwidth spam from client settings changes

--- fabric/src/main/java/com/noxcrew/noxesium/core/fabric/feature/SyncGuiScale.java
+++ fabric/src/main/java/com/noxcrew/noxesium/core/fabric/feature/SyncGuiScale.java
@@ -5,6 +5,7 @@ import com.noxcrew.noxesium.api.network.NoxesiumServerboundNetworking;
 import com.noxcrew.noxesium.core.feature.ChatVisibility;
 import com.noxcrew.noxesium.core.feature.ClientSettings;
 import com.noxcrew.noxesium.core.network.serverbound.ServerboundClientSettingsPacketV2;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.Minecraft;
 
 /**
@@ -12,6 +13,44 @@ import net.minecraft.client.Minecraft;
  * client information.
  */
 public class SyncGuiScale extends NoxesiumFeature {
+
+    private boolean requested = false;
+    private int requestedCooldown = 0;
+
+    public SyncGuiScale() {
+        ClientTickEvents.END_CLIENT_TICK.register((ignored) -> {
+            if (!isRegistered()) return;
+
+            // Ignore if not requested
+            if (!requested || requestedCooldown-- > 0) return;
+            requested = false;
+            requestedCooldown = 20;
+
+            // Don't send if there is no established connection
+            if (Minecraft.getInstance().getConnection() == null) return;
+
+            var window = Minecraft.getInstance().getWindow();
+            var options = Minecraft.getInstance().options;
+
+            NoxesiumServerboundNetworking.send(new ServerboundClientSettingsPacketV2(new ClientSettings(
+                    options.guiScale().get(),
+                    window.getGuiScale(),
+                    window.getGuiScaledWidth(),
+                    window.getGuiScaledHeight(),
+                    Minecraft.getInstance().isEnforceUnicode(),
+                    options.touchscreen().get(),
+                    options.notificationDisplayTime().get(),
+                    switch (options.chatVisibility().get()) {
+                        case FULL -> ChatVisibility.FULL;
+                        case HIDDEN -> ChatVisibility.HIDDEN;
+                        case SYSTEM -> ChatVisibility.SYSTEM;
+                    },
+                    options.chatWidth().get(),
+                    options.chatHeightUnfocused().get(),
+                    options.fov().get(),
+                    options.fovEffectScale().get())));
+        });
+    }
 
     @Override
     public void onRegister() {
@@ -23,28 +62,6 @@ public class SyncGuiScale extends NoxesiumFeature {
      * allows servers to more accurately adapt their UI to clients.
      */
     public void syncGuiScale() {
-        // Don't send if there is no established connection
-        if (Minecraft.getInstance().getConnection() == null) return;
-
-        var window = Minecraft.getInstance().getWindow();
-        var options = Minecraft.getInstance().options;
-
-        NoxesiumServerboundNetworking.send(new ServerboundClientSettingsPacketV2(new ClientSettings(
-                options.guiScale().get(),
-                window.getGuiScale(),
-                window.getGuiScaledWidth(),
-                window.getGuiScaledHeight(),
-                Minecraft.getInstance().isEnforceUnicode(),
-                options.touchscreen().get(),
-                options.notificationDisplayTime().get(),
-                switch (options.chatVisibility().get()) {
-                    case FULL -> ChatVisibility.FULL;
-                    case HIDDEN -> ChatVisibility.HIDDEN;
-                    case SYSTEM -> ChatVisibility.SYSTEM;
-                },
-                options.chatWidth().get(),
-                options.chatHeightUnfocused().get(),
-                options.fov().get(),
-                options.fovEffectScale().get())));
+        requested = true;
     }
 }
